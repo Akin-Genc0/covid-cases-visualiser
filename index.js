49180f21af4a051444d24b6dd39103ac8e011e27
Akin-Genc0 <akingenc212@gmail.com>
2025-05-17 15:32:40 +0100

    Set up country input and fetch COVID data from API

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const countryInput = document.querySelector('#country');
 const addBtn = document.querySelector('#add');
 
 
+async function getData(){
 
+try{
+
+const countryName = countryInput.value.toLowerCase();
+const resp = await fetch(`https://disease.sh/v3/covid-19/countries/${countryName}`)
+
+if(!resp.ok){
+    throw new Error("Not found")
+}
+
+const data = await resp.json();
+console.log(data);
+
+} catch(error) {
+    console.error(error);
+    
+}
+}
 
 
 
 
 addBtn.addEventListener('click', function(){
 
-
+getData();
 
 });
