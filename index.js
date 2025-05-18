826468ecdbacdda7ffbeb0f96f938f32bffe769f
Akin-Genc0 <akingenc212@gmail.com>
2025-05-18 12:37:13 +0100

    Complete dynamic chart feature with country case tracking and Chart.js integration

--- index.js
+++ index.js
@@ -1,55 +1,87 @@
 const countryInput = document.querySelector('#country');
 const addBtn = document.querySelector('#add');
-
+const ctx = document.getElementById('myChart').getContext('2d'); 
 
 let countryData = [];
+let chartInstance;
 
-function vaild(){
-    if(countryInput.value === ""){
-        alert("Enter value");
-    }else {
-        return true;
+function vaild() {
+  if (countryInput.value === "") {
+    alert("Enter value");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+async function getData() {
+  try {
+    const countryName = countryInput.value.toLowerCase();
+    const resp = await fetch(`https://disease.sh/v3/covid-19/countries/${countryName}`);
+
+    if (!resp.ok) {
+      throw new Error("Not found");
     }
-}
 
+    const data = await resp.json();
+    console.log(data);
 
+    const conName = data.country;
+    const cases = data.cases;
 
-async function getData(){
+    let obj = {
+      country: conName,
+      case: cases
+    };
 
-try{
+    countryData.push(obj);
+    console.log(countryData);
 
-const countryName = countryInput.value.toLowerCase();
-const resp = await fetch(`https://disease.sh/v3/covid-19/countries/${countryName}`)
+  
+    let n = countryData.map((a) => a.country);
+    let c = countryData.map((a) => a.case);
 
-if(!resp.ok){
-    throw new Error("Not found")
-}
+  
+    if (chartInstance) {
+      chartInstance.destroy();
+    }
 
-const data = await resp.json();
-console.log(data);
+    chartInstance = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: n,
+        datasets: [{
+          label: 'Covid-19 Cases',
+          data: c,
+          borderWidth: 1,
+          backgroundColor: 'rgba(75, 192, 192, 0.5)'
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
 
-
-const conName = data.country;
-const cases = data.cases;
-
-let obj =  {
-    country: conName,
-    case: cases
-}
-
-countryData.push(obj);
-console.log(countryData)
-} catch(error) {
+  } catch (error) {
     console.error(error);
-    
+  }
 }
-    }
 
-
-addBtn.addEventListener('click', function(){
-
-    if(!vaild()) return;
-    
-    getData();
-
+addBtn.addEventListener('click', function () {
+  if (!vaild()) return;
+  getData();
 });
+
+
+
+
+
+
+
+
+
+
